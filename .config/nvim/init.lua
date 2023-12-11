cba75043237c40bac2db614d7b3991fc2f9a48b6
willzhang4a58 <willzhang4a58@gmail.com>
2023-12-11 16:37:43 +0800

    add mason

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,4 +10,7 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup()
+require("lazy").setup({
+  "williamboman/mason.nvim",
+})
+require("mason").setup()
